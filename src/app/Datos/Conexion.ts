71767bb3f7ce75a34ae11e3d01ddddbf8423005d
jgarcia.ilm@outlook.com <jgarcia.ilm@outlook.com>
2022-02-04 15:39:47 -0800

    Sync 4

---
+++ src/app/Datos/Conexion.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import Swal from 'sweetalert2';
+import { HttpClient,HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+    providedIn:'root'
+})
+export class ApiRuta{
+  constructor(private http:HttpClient){}
+
+    //serverUrl="http://10.35.64.27:9080/";
+
+    serverUrl="https://localhost:44352/";
+    //serverUrl="http://localhost:8080/";
+
+    headers= new HttpHeaders()
+    .set('Access-Control-Allow-Credentials' , 'true')
+    .set('Access-Control-Allow-Origin', '*')
+    .set('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, OPTIONS')
+    .set('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With');
+
+
+    api(){
+        return this.serverUrl;
+    }
+    messaggeError(data: any) {
+        Swal.fire({
+          position: 'center',
+          icon: 'error',
+          title: 'DATABASE ERROR',
+          text: JSON.stringify(data),
+          footer: '<a href="https://apps.hunterdouglas.com/SupportRequest/request" target="_blank">Report To IT Developers</a>'
+        })
+      }
+  }
+  
