d783080b8f995e65974e5a80eea601bd5b498b00
jgarcia.ilm@outlook.com <jgarcia.ilm@outlook.com>
2022-02-04 15:16:56 -0800

    sync 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MenuComponent } from './Menu/menu.component';
+import { InicioComponent } from './Inicio/inicio.component';
+import { NotfoundComponent } from './NotFound/notfound.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : '', redirectTo: '/inicio', pathMatch: 'full'},
+  {path : 'inicio', component:InicioComponent},
+  {path : 'menu', component:MenuComponent},
+  {path : '**', component:NotfoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,41 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './Menu/menu.component';
+import { InicioComponent } from './Inicio/inicio.component';
+import { NotfoundComponent } from './NotFound/notfound.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule,  ReactiveFormsModule } from '@angular/forms';
+import { MatNativeDateModule} from '@angular/material/core';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    InicioComponent,
+    NotfoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    NgbModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    MatNativeDateModule,
+    MatTooltipModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatProgressSpinnerModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
